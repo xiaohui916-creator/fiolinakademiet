1acbf36b6df23c4bc8f40cbde84c54ee505a7a58
Wenting Xu <wentingx@ru.is>
2026-06-11 13:23:06 +0200

    fix imports in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Hero from './components/Hero';
 import OmOss from './components/OmOss';
 import Aktiviteter from './components/Aktiviteter';
 import Larere from './components/Larere';
-import Kontakt from './components/Kontakt';
 import Sponsorer from './components/Sponsorer';
+import Kontakt from './components/Kontakt';
 
 export default function Home() {
   return (
